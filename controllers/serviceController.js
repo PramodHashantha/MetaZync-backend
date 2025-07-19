fafc3c36bd4002a77681f81f522d9d32ea3b991d
Nilushka169 <s22009961@ousl.lk>
2025-07-20 01:44:25 +0530

    service managing conrtollers added

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,12 +1,46 @@
 const Service = require('../models/Service');
 
+// Get all services
 exports.getServices = async (req, res) => {
-  const services = await Service.find();
-  res.json(services);
+  try {
+    const services = await Service.find();
+    res.json(services);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
+// Create new service
 exports.createService = async (req, res) => {
-  const newService = new Service({ name: req.body.name });
-  await newService.save();
-  res.status(201).json(newService);
+  try {
+    const newService = new Service({ name: req.body.name });
+    await newService.save();
+    res.status(201).json(newService);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Update service
+exports.updateService = async (req, res) => {
+  try {
+    const updatedService = await Service.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
+    res.json(updatedService);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete service
+exports.deleteService = async (req, res) => {
+  try {
+    await Service.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Service deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
