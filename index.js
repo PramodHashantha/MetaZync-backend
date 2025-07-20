d1ae85bad36f97a8b724f7f2898d1ee66dbbe908
Nilushka169 <s22009961@ousl.lk>
2025-07-20 15:23:08 +0530

    port added into index, json is updated

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const serverless = require('serverless-http');
 
 // Load env variables
 dotenv.config();
 
 // Connect to MongoDB
-// connectDB();
+connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://frontend-or7r.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Root route for testing in browser
@@ -25,8 +32,16 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/services', require('./routes/serviceRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
+
 // Export for Vercel
-module.exports = serverless(app);
+// module.exports = serverless(app);
 
 
 
