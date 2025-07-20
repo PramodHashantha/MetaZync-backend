1141f9a26c4183548dc3af7dc9eb8b54a609d36b
Nilushka169 <s22009961@ousl.lk>
2025-07-20 15:09:32 +0530

    index / path set, vercel.json changed

--- index.js
+++ index.js
@@ -15,10 +15,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root route for testing in browser
+app.get('/', (req, res) => {
+  res.send('🚀 Backend deployed successfully on Vercel!');
+});
+
 // API routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/services', require('./routes/serviceRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 
-// Export properly for Vercel
+// Export for Vercel
 module.exports = serverless(app);
+
+
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const connectDB = require('./config/db');
+// const serverless = require('serverless-http');
+
+
+// dotenv.config();
+
+
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/services', require('./routes/serviceRoutes'));
+// app.use('/api/bookings', require('./routes/bookingRoutes'));
+
+
+// module.exports = serverless(app);
